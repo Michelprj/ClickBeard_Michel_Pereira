7c1474d7d1635a54900ebee54e1496c2aa0e0500
Michel Pereira <michelpr1224@gmail.com>
2024-07-25 20:49:24 -0300

    refactor: applied data source configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,26 +1,19 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
-import { User } from './modules/user/entities/user.entity';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/guards/auth.guard';
-import 'dotenv/config';
+import { DatabaseModule } from './database/database.module';
+import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/entities/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      entities: [User],
-      synchronize: true,
-    }),
+    TypeOrmModule.forFeature([User]),
+    DatabaseModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
@@ -33,7 +26,7 @@ import 'dotenv/config';
     AuthModule,
     UserModule,
   ],
-  controllers: [],
+  controllers: [AppController],
   providers: [
     {
       provide: APP_GUARD,
