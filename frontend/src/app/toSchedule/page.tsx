a86e4eb634e5fa9cf678014dead856f76d1b65f4
Michel Pereira <michelpr1224@gmail.com>
2024-07-28 00:47:58 -0300

    feat: added header in toSchedule page

--- frontend/src/app/toSchedule/page.tsx
+++ frontend/src/app/toSchedule/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import ComponentsAppsCalendar from "@/components/calendar";
+import Header from "@/components/interface/header";
 
 export default function ToSchedule() {
   return (
-    <div className="min-h-screen bg-black px-12">
+    <div className="min-h-screen bg-[#181717]">
+      <Header />
       <ComponentsAppsCalendar />
     </div>
   )
